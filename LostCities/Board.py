184b098d8d44086a83646e844b840d4fd7d2391a
farkas1070 <71120991+farkas1070@users.noreply.github.com>
2023-04-20 06:20:13 +0200

    installed pre commit, moved docs

--- LostCities/Board.py
+++
@@ -1,6 +0,0 @@
-class Game():
-    def __init__(self, discardpile,playercardpile,drawpile):
-        self.discardpile = discardpile
-        self.playercardpile = playercardpile
-        self.drawpile = drawpile
-        
